2d17967381aa6df68833fb9559f7e7c83fea5392
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2015-09-10 22:02:38 -0500

    added todo creation functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,23 @@
 class ItemsController < ApplicationController
 
   def create
+    @user = current_user
     @item = current_user.items.create(item_params)
     if @item.save
-      
+      flash.now[:notice] = "The item was saved successfully."
+      redirect_to user_path
     else
-    
+      flash[:error] = "There was an error saving the item."
+      redirect_to user_path
     end
+  end
+
+  def destroy
   end
   
   private
 
   def item_params
-
+    params.require(:item).permit(:name)
   end
 end
